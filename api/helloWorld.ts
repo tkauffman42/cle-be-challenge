e209ce34f94a01b38128bcf4de969438711a2fc5
tkauffman <troykauffman1@gmail.com>
2021-12-17 09:00:08 -0500

    changing the helloword for a test

--- api/helloWorld.ts
+++ api/helloWorld.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = "Worlds" } = request.query;
+  const { name = "Troy" } = request.query;
   response.status(200).send(`Hello ${name}!`);
 };
