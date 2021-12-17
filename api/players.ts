849122ee6b2558cba898618c809a42c22dca4821
tkauffman <troykauffman1@gmail.com>
2021-12-17 08:55:55 -0500

    changed the declaring statements

--- api/players.ts
+++ api/players.ts
@@ -2,8 +2,8 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { playerOverviews } from "../data/PlayerOverviews"
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = playerOverviews } = request.body;
-
+  const playerOverview = playerOverviews;
+  
   //response.status(200).send(`Hello ${name}!  J`);
-  response.status(200).send(`${name}`)
+  response.status(200).json(`${playerOverview}`)
 };
