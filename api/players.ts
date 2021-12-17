6be93abf7b82cede9de08bc74befca0dd8b112a1
tkauffman <troykauffman1@gmail.com>
2021-12-17 08:57:53 -0500

    removed the setting as a variable

--- api/players.ts
+++ api/players.ts
@@ -2,8 +2,7 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { playerOverviews } from "../data/PlayerOverviews"
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const playerOverview = playerOverviews;
   
   //response.status(200).send(`Hello ${name}!  J`);
-  response.status(200).json(`${playerOverview}`)
+  response.status(200).json(`${playerOverviews}`)
 };
