712875c60a066ce943ae0fbecbfb5de338c0a2d5
tkauffman <troykauffman1@gmail.com>
2021-12-17 07:43:39 -0500

    Added the playerOverviews api

---
+++ api/players.ts
@@ -0,0 +1,7 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { playerOverviews } from "../data/PlayerOverviews"
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { name = playerOverviews } = request.query;
+  response.status(200).send(`Hello ${name}!`);
+};
