be620cbb702463477785dc3a9c003b98a9f16fb4
tkauffman <troykauffman1@gmail.com>
2021-12-17 08:40:14 -0500

    changed to send name

--- api/players.ts
+++ api/players.ts
@@ -2,7 +2,8 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { playerOverviews } from "../data/PlayerOverviews"
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = playerOverviews } = request.query;
+  const { name = playerOverviews } = request.body;
+
   //response.status(200).send(`Hello ${name}!  J`);
-  response.status(200).json(`${name}`)
+  response.status(200).send(`${name}`)
 };
