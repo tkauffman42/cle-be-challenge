e819c2cd02f7fe46bfd52454e9908715d46ec222
tkauffman <troykauffman1@gmail.com>
2021-12-19 10:52:21 -0500

    trying the json stringify

--- api/players.ts
+++ api/players.ts
@@ -4,5 +4,5 @@ import { playerOverviews } from "../data/PlayerOverviews"
 export default (request: VercelRequest, response: VercelResponse) => {
   
   //response.status(200).send(`Hello ${name}!  J`);
-  response.status(200).json(`${playerOverviews}`)
+  response.status(200).json(JSON.stringify(`${playerOverviews}`))
 };
