f94d298ab3b5fc6dfc04afe9298ff1dbb0d53525
tkauffman <troykauffman1@gmail.com>
2021-12-29 13:55:34 -0500

    had to remove the foreach and replace it with for

--- api/players.ts
+++ api/players.ts
@@ -2,23 +2,19 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { playerOverviews } from "../data/PlayerOverviews"
 
 module.exports = (req, res) => {
-    const objectPlayers = playerOverviews;
-    const startPlayer = {"players":objectPlayers};
-      
+
     const playerId = Number(req.query.playerId);
     
     try {
         if(playerId){
-            objectPlayers.forEach(function(element, i) {
-                if(element.playerId === playerId){
-                    const playerDetail = {"playerDetail": element}
-                    res.status(200).json(playerDetail)
-                }else if (i === objectPlayers.length - 1){
-                    res.send("You have entered an incorrect playerId")
+            for(let i =0; i < playerOverviews.length; i++){
+                if(playerOverviews[i].playerId === playerId){
+                    return res.json({playerDetail: playerOverviews[i]});
                 }
-            });
+            }
+            return res.json({message: "incorrect playerId"});
         }else{
-            res.status(200).json(startPlayer);
+            res.status(200).json({players:playerOverviews});
         }
     } catch (error) {
         res.send(error)
