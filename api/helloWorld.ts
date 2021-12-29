e0e31af3d946b35ae00fcb3dbb09c07f0568f1ad
tkauffman <troykauffman1@gmail.com>
2021-12-29 11:10:05 -0500

    Worked on fixing the query of player information

--- api/helloWorld.ts
+++ api/helloWorld.ts
@@ -1,6 +1,33 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = "Troy" } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+
+const player = [
+
+  {
+    id: 1,
+    fullName: "Troy Kauffman",
+    throws: "right"
+  },
+  {
+    id: 2,
+    fullName: "Jeremy Kauffman",
+    throws: "left"
+  }
+];
+  
+  var number = Number(request.query.id)
+
+try {
+  if(request.query.id){
+    player.forEach(function(element){
+      if(element.id === number){
+        response.status(200).json(element);
+      }
+    })
+  }
+} catch (error) {
+  response.status(200).send(error);
+}
+
 };
--- api/players.ts
+++ api/players.ts
@@ -1,8 +1,28 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { playerOverviews } from "../data/PlayerOverviews"
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  
-  //response.status(200).send(`Hello ${name}!  J`);
-  response.status(200).json(JSON.stringify(`${playerOverviews}`))
-};
+module.exports = (req, res) => {
+    const objectPlayers = playerOverviews;
+    //const startPlayer = {"players":objectPlayers};
+    
+    
+    const playerId = Number(req.query.playerId);
+
+    //console.log(req.query.playerId)
+    
+    try {
+        if(playerId){
+            objectPlayers.forEach(function(element) {
+                if(element.playerId === playerId){
+                    const playerDetail = {"playerDetail": element}
+                    res.status(200).json(playerDetail)
+                }
+            });
+        }
+    } catch (error) {
+        res.send(error)
+    }
+
+
+   // res.status(200).json(startPlayer);
+ }
