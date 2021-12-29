d394a00ff8cadc328569206f6e07e8cb22cd9806
tkauffman <troykauffman1@gmail.com>
2021-12-29 12:52:49 -0500

    removed comments added response error if user enters an incorrect playerId

--- api/players.ts
+++ api/players.ts
@@ -4,29 +4,23 @@ import { playerOverviews } from "../data/PlayerOverviews"
 module.exports = (req, res) => {
     const objectPlayers = playerOverviews;
     const startPlayer = {"players":objectPlayers};
-    
-    
+      
     const playerId = Number(req.query.playerId);
-
-    //console.log(req.query.playerId)
     
     try {
         if(playerId){
-            objectPlayers.forEach(function(element) {
+            objectPlayers.forEach(function(element, i) {
                 if(element.playerId === playerId){
-                    console.log("inside the if")
                     const playerDetail = {"playerDetail": element}
                     res.status(200).json(playerDetail)
+                }else if (i === objectPlayers.length - 1){
+                    res.send("You have entered an incorrect playerId")
                 }
             });
         }else{
-            console.log("inside the else");
             res.status(200).json(startPlayer);
         }
     } catch (error) {
         res.send(error)
     }
-
-
-   // res.status(200).json(startPlayer);
  }
