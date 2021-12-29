6bf9753c27f5c3dd7747dfaad18360e2f1a8a053
tkauffman <troykauffman1@gmail.com>
2021-12-29 12:36:02 -0500

    forgot the else catch if no one is selected

--- api/players.ts
+++ api/players.ts
@@ -3,7 +3,7 @@ import { playerOverviews } from "../data/PlayerOverviews"
 
 module.exports = (req, res) => {
     const objectPlayers = playerOverviews;
-    //const startPlayer = {"players":objectPlayers};
+    const startPlayer = {"players":objectPlayers};
     
     
     const playerId = Number(req.query.playerId);
@@ -14,10 +14,14 @@ module.exports = (req, res) => {
         if(playerId){
             objectPlayers.forEach(function(element) {
                 if(element.playerId === playerId){
+                    console.log("inside the if")
                     const playerDetail = {"playerDetail": element}
                     res.status(200).json(playerDetail)
                 }
             });
+        }else{
+            console.log("inside the else");
+            res.status(200).json(startPlayer);
         }
     } catch (error) {
         res.send(error)
