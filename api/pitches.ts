ea8e4949cf1d4f55b2651936ae38e82541feebcf
tkauffman <troykauffman1@gmail.com>
2022-01-01 15:10:49 -0500

    removed not needed imports changed from looping all pitches to just filter

--- api/pitches.ts
+++ api/pitches.ts
@@ -1,30 +1,21 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { pitches } from "../data/Pitches"
-import { playerOverviews } from "../data/PlayerOverviews";
-import { playerDetails} from "../data/PlayerDetails";
+
 
 module.exports = (req, res) => {
 
     const playerId = Number(req.query.playerId);
-    const pitchesThrown = [];
     try {
         if(playerId){
-            for(let i =0; i < pitches.length; i++){
-                if(pitches[i].pitcherId === playerId){
-                    //return res.json({playerDetail: playerDetails[i]});
-                    pitchesThrown.push(pitches[i])
-                }
+            const pitchData = pitches.filter(p => p.pitcherId === playerId);
+            if(pitchData.length < 1){
+                return res.json({message: "please enter a valid playerId"});
             }
-            //console.log(pitchesThrown);
-            if (pitchesThrown.length < 1){
-                return res.json({message: "incorect playerId"});
-            }else{
-                return res.json({pitches:pitchesThrown});
-            }
+            return res.status(200).json({pitches: pitchData});
         }else{
-            res.json({message:"a playerId is required"});
+            return res.json({message: "A playerId is required"})
         }
     } catch (error) {
-        res.send(error)
+        res.send(error);
     }
  }
